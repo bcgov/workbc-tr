7f9952167bb2a776c80c243cd15781ace985bb09
sonal.bendle <sonal.bendle@cgi.com>
2021-07-05 23:58:00 +0000

    removed extra DB entry

--- src/web/sites/default/settings.php
+++ src/web/sites/default/settings.php
@@ -90,17 +90,6 @@
  */
 $databases = [];
 
-$databases['default']['default'] = array (
-  'database' => 'mst_dev',
-  'username' => 'teachers',
-  'password' => 'Gr1ml0ck',
-  'prefix' => '',
-  'host' => 'localhost',
-  'port' => '5432',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\pgsql',
-  'driver' => 'pgsql',
-);
-
 /**
  * Customizing database settings.
  *
