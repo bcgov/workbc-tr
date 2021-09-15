8f94e596ab738232cef10335f55ed1544bcd582c
sonalfulkar <sonal.bendle@gmail.com>
2021-09-15 00:56:12 +0000

    Download feature - fixes

--- src/web/sites/default/settings.php
+++ src/web/sites/default/settings.php
@@ -89,7 +89,16 @@
  * @endcode
  */
 $databases = [];
-
+$databases['default']['default'] = array (
+  'database' => 'mst_dev',
+  'username' => 'teachers',
+  'password' => 'Gr1ml0ck',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '5432',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\pgsql',
+  'driver' => 'pgsql',
+);
 /**
  * Customizing database settings.
  *
