44ef102640080d4c9926341e0504e3383dd84d03
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2025-05-20 16:22:15 -0700

    Add files via upload

---
+++ terraform/dev/security.tf
@@ -0,0 +1,43 @@
+# security.tf
+
+# ALB Security Group: Edit to restrict access to the application
+data "aws_security_group" "web" {
+  name = "Web_sg"
+}
+
+data "aws_security_group" "app" {
+  name = "App_sg"
+}
+
+data "aws_security_group" "data" {
+  name = "Data_sg"
+}
+
+# Traffic to the ECS cluster should only come from the ALB
+/*
+data "aws_security_group" "ecs_tasks" {
+  name        = "workbc-cc-ecs-tasks-security-group"
+}
+
+
+data "aws_security_group" "efs_security_group" {
+  name        = "workbc-cc-efs-security-group"
+}*/
+
+resource "aws_security_group" "allow_nfs" {
+  name        = "allow_nfs"
+  description = "Allow NFS inbound traffic and all outbound traffic"
+  #vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "allow_nfs"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_nfs_ipv4" {
+  security_group_id = aws_security_group.allow_nfs.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 2049
+  ip_protocol       = "tcp"
+  to_port           = 2049
+}
