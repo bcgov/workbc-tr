ff10b26198fb66a8ef839869b28b70246718c765
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2025-05-21 11:32:05 -0700

    Update security.tf

--- terraform/dev/security.tf
+++ terraform/dev/security.tf
@@ -40,3 +40,9 @@ resource "aws_vpc_security_group_ingress_rule" "allow_nfs_ipv4" {
   ip_protocol       = "tcp"
   to_port           = 2049
 }
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_nfs.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
