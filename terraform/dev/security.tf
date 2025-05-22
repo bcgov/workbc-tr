556f86658e3ca6113a7c0f6a7dfa0f9643e1e9ea
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2025-05-22 09:06:26 -0700

    Update security.tf

--- terraform/dev/security.tf
+++ terraform/dev/security.tf
@@ -46,3 +46,27 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
+
+resource "aws_security_group" "allow_postgres" {
+  name        = "allow_postgres"
+  description = "Allow Postgres inbound traffic and all outbound traffic"
+  vpc_id      = module.network.aws_vpc.id
+
+  tags = {
+    Name = "allow_postgres"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_postgres_ipv4" {
+  security_group_id = aws_security_group.allow_postgres.id
+  cidr_ipv4         = module.network.aws_vpc.cidr_block
+  from_port         = 2049
+  ip_protocol       = "tcp"
+  to_port           = 2049
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4a" {
+  security_group_id = aws_security_group.allow_postgres.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
