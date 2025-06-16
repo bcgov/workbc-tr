46de18580594b0c6a982b5c98c3357c97dad00b2
Jack Tang <jinchuant@gmail.com>
2025-06-16 10:43:20 -0700

    file location

--- terraform/test/policy.tf
+++ terraform/test/policy.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_policy" "alb_policy" {
   name   = "AWSLoadBalancerControllerIAMPolicy"
-  policy = file("${path.module}/iam_policy.json")
+  policy = file("../iam_policy.json")
 }
 
