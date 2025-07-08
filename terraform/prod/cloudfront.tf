dfd8bdc7b8afd8b421356572daee8ae1aa4e65bd
Jack Tang <jinchuant@gmail.com>
2025-07-08 10:25:05 -0700

    CloudFront

---
+++ terraform/prod/cloudfront.tf
@@ -0,0 +1,88 @@
+# cloudfront.tf
+resource "random_integer" "cf_origin_id" {
+  min = 1
+  max = 100
+}
+
+resource "aws_cloudfront_distribution" "workbc-cer" {
+
+  count = var.cloudfront ? 1 : 0
+
+  origin {
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols = [
+      "TLSv1.2"]
+    }
+
+    domain_name = "k8s-app-cerappin.b89n0c-prod.nimbus.cloud.gov.bc.ca"
+    origin_id   = random_integer.cf_origin_id.result
+	
+	custom_header {
+	  name = "X-Forwarded-Host"
+	  value = "careereducation2.workbc.ca"
+	}
+	
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+  comment         = "Career Education Resources"
+
+  default_cache_behavior {
+    allowed_methods = [
+      "DELETE",
+      "GET",
+      "HEAD",
+      "OPTIONS",
+      "PATCH",
+      "POST",
+    "PUT"]
+    cached_methods = ["GET", "HEAD"]
+
+    target_origin_id = random_integer.cf_origin_id.result
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "all"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+	
+    # SimpleCORS
+    response_headers_policy_id = "60669652-455b-4ae9-85a4-c4c02393f86c"
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations = ["CA"]
+    }
+  }
+
+  tags = var.common_tags
+  
+  aliases = ["careereducation2.workbc.ca"]
+
+  viewer_certificate {
+    acm_certificate_arn = "arn:aws:acm:us-east-1:054099626264:certificate/9ccc0657-fc54-4295-8d18-2c09e1d7392f"
+    minimum_protocol_version = "TLSv1.2_2021"
+    ssl_support_method = "sni-only"
+  }
+}
+
+output "cloudfront_url2" {
+  value = "https://${aws_cloudfront_distribution.workbc-cer[0].domain_name}"
+
+}
+
