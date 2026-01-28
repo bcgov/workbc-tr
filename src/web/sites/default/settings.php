07c7af38a4abada055dd02601f1e36231eff9ba8
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2026-01-28 14:40:33 -0800

    Update settings.php

--- src/web/sites/default/settings.php
+++ src/web/sites/default/settings.php
@@ -722,16 +722,14 @@ $settings['trusted_host_patterns'] = [
   '^workbc-tr.docker.localhost',
   '^careereducation-dev.workbc.ca',
   '^careereducation-dev2\.workbc\.ca$',
-  '^k8s-demo-myapping\.b89n0c-dev\.nimbus\.cloud\.gov\.bc\.ca$',
   '^careereducation-test.workbc.ca',
   '^careereducation-test2\.workbc\.ca$',
-  '^k8s-app-cerappin\.b89n0c-test\.nimbus\.cloud\.gov\.bc\.ca$',
   '^careereducation.workbc.ca',
   '^careereducation2\.workbc\.ca$',
-  '^k8s-app-cerappin\.b89n0c-prod\.nimbus\.cloud\.gov\.bc\.ca$',
   '^workbc-cer\.a55eb5-dev\.stratus\.cloud\.gov\.bc\.ca$',
   '^workbc-cer\.a55eb5-test\.stratus\.cloud\.gov\.bc\.ca$',
-  '^workbc-cer\.a55eb5-prod\.stratus\.cloud\.gov\.bc\.ca$'
+  '^workbc-cer\.a55eb5-prod\.stratus\.cloud\.gov\.bc\.ca$',
+  '^cer-app-service$'
 ];
 
 /**
