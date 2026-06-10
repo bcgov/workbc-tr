80a2e38824567fb3d280cd6a48241b3c44d9efcb
vmuchalla <148374832+vmuchalla@users.noreply.github.com>
2026-06-10 10:26:57 -0700

    Update cloudfront.tf

--- terraform/test/cloudfront.tf
+++ terraform/test/cloudfront.tf
@@ -75,7 +75,7 @@ resource "aws_cloudfront_distribution" "workbc-cer" {
   aliases = ["careereducation-test.workbc.ca"]
 
   viewer_certificate {
-    acm_certificate_arn = "arn:aws:acm:us-east-1:054099626264:certificate/9ccc0657-fc54-4295-8d18-2c09e1d7392f"
+    acm_certificate_arn = "arn:aws:acm:us-east-1:318574063652:certificate/6c9be00f-eb33-4bdb-a388-02c6f7d2c1d5"
     minimum_protocol_version = "TLSv1.2_2021"
     ssl_support_method = "sni-only"
   }
